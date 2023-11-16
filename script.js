29aa5047a7b37dff7042a1a63090ef8718208198
JOEBRO324 <67446564+JOEBRO324@users.noreply.github.com>
2023-11-16 00:00:27 -0600

    Create script.js
    changed name

---
+++ script.js
@@ -0,0 +1,8 @@
+(function () {
+    "use strict";
+    var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+    var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+    return new bootstrap.Dropdown(dropdownToggleEl)
+})
+
+})();
